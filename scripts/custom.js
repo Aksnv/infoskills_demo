e632f53b6e549f915ab192d62389c07ee99e1ace
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-05-24 10:21:13 +0300

    create repository

---
+++ scripts/custom.js
@@ -0,0 +1,70 @@
+//	Scroll to top
+
+$(window).scroll(function(){
+  var $scrollup = $('.back-to-top');
+  if ($(this).scrollTop() > 120) { $scrollup.fadeIn(); $('header').addClass("sticky"); }
+  else { $scrollup.fadeOut(); $('header').removeClass("sticky"); }
+  
+});
+
+jQuery('.back-to-top').click(function () {
+  jQuery("html, body").animate({
+	  scrollTop: 0
+  }, 600);
+  return false;
+});	  
+	  
+
+$(document).click(function (e){ // событие клика по веб-документу
+	var div = $("#menu-click");
+	var div2 = $("#menu-click-list");
+	var inp1 = $("input.btn-search-click");
+	var inp2 = $("input.btn_close-click");
+	var form1 = $("div.search-click-slide-form");	
+	if (!div.is(e.target) && div.has(e.target).length === 0 && div.prop("checked") && !div2.is(e.target) && div2.has(e.target).length === 0)	{ 
+		    div.removeAttr("checked");
+		}
+	if (!form1.is(e.target) && form1.has(e.target).length === 0 && inp1.prop("checked") && !inp2.prop("checked") && !inp1.is(e.target) && inp1.has(e.target).length === 0)	{ 
+		    inp1.removeAttr("checked");
+		}
+
+	if (inp1.prop("checked") && !inp2.prop("checked") && inp2.is(e.target) && inp2.has(e.target).length === 0)	{ 
+		    inp1.removeAttr("checked");
+		}		
+		
+});
+
+
+// Choose payment method
+
+$(document).click(function (e){
+
+	$('li.pkp-payment-method__list-item a').click(function(){
+		var tx = $(this).html();
+		var tv = $(this).attr('href');
+		tv = tv.replace('#','');
+		$(".pkp-payment-method__choose").html(tx);
+		$("input.pkp-payment-method__btn").removeAttr("checked");
+		$(".pkp-payment-method__item").each(function(indx){
+			if($(this).hasClass("active")){	
+				$(this).removeClass("active");
+			}			  
+			if($(this).hasClass(tv)){	
+				$(this).addClass("active");
+			}
+		});
+	});
+})
+
+// Add review form
+
+$(".add-review-button").click(function(e) {
+	e.preventDefault();
+	$(".add-review-form").toggleClass("add-review-form--open");
+	$(this).hide();
+});
+
+$(".add-review-form__reset").click(function() {
+	$(".add-review-form").toggleClass("add-review-form--open");
+	$(".add-review-button").show();
+});
